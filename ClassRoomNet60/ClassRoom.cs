b9b5a87c487b2c575d1a02a7349506c72ab60184
Christian Wessel Ammentorp <cha007@edu.zealand.dk>
2026-08-31 10:37:57 +0200

    Created ClassRoom class

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class ClassRoom
+{
+    public string Name { get; set; }
+    public List<Student> Students { get; set; }
+    public DateTime SemesterStart { get; set; }
+
+    public ClassRoom()
+    {
+        Name = string.Empty;
+        Students = new List<Student>();
+        SemesterStart = DateTime.MinValue;
+    }
+}
+
