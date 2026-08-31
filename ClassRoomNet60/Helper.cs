77717c215f8e7726f7dc58c0ae3e495c0ef2f9a2
Christian Wessel Ammentorp <cha007@edu.zealand.dk>
2026-08-31 11:17:00 +0200

    Created helper method for getting season from month

---
+++ ClassRoomNet60/Helper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static class Helper
+{
+    public static string MonthToSeason(int month)
+    {
+        switch (month % 12)
+        {
+            case 12: case 1: case 2:
+                return "Winter";
+            case 3: case 4: case 5:
+                return "Spring";
+            case 6: case 7: case 8:
+                return "Summer";
+            case 9: case 10: case 11:
+                return "Autumn";
+            default:
+                return "Winter";
+        }
+    }
+    public static string MonthToSeason(DateTime dateTime)
+    {
+        return MonthToSeason(dateTime.Month);
+    }
+}
+
