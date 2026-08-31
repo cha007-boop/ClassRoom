3e98edef5ee0cdc4e887c3c6e789b16fc722d74f
Christian Wessel Ammentorp <cha007@edu.zealand.dk>
2026-08-31 10:26:25 +0200

    Created Student class

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Student
+{
+    private int _birthMonth;
+    private int _birthDay;
+    
+    public string Name { get; private set; }
+    public int BirthMonth
+    {
+        get { return _birthMonth; }
+        private set { _birthMonth = value; }
+    }
+    public int BirthDay
+    {
+        get { return _birthDay; }
+        private set { _birthDay = value; }
+    }
+    public Student(string name, int birthMonth, int birthDay)
+    {
+        Name = name;
+        BirthMonth = birthMonth;
+        BirthDay = birthDay;
+    }
+}
+
